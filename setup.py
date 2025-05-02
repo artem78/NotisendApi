4dc58757a3e3807cca861c08ba639fff9c9f7212
artem78 <megabyte1024@ya.ru>
2025-05-02 13:12:14 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name="NotisendApi",
+    description='Send mails with notisend.ru API',
+    version='1.0',
+    author='artem78',
+    #packages=['ваш пакет'],
+    py_modules=['notisend_api'],
+    install_requires=['requests'],
+    url='https://github.com/artem78/NotisendApi'
+)
